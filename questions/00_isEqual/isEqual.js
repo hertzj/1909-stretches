fa7dd0047aa495556b55125e27f1acccac79de6f
Jake Hertz <hertzjake@gmail.com>
2019-09-27 10:23:00 -0400

    stretch solution isEqual

--- questions/00_isEqual/isEqual.js
+++ questions/00_isEqual/isEqual.js
@@ -3,6 +3,33 @@ function isEqual(obj1, obj2) {
   // run your tests by running `jest isEqual`
   // you can also run tests on every save by running
   // `jest --watch isEqual`
+
+  const firstKeys = Object.keys(obj1);
+  const secondKeys = Object.keys(obj2);
+  let limit = 0;
+
+
+  if (firstKeys.length >= secondKeys.length) {
+    limit = firstKeys.length;
+  }
+  else {
+    limit = secondKeys.length;
+  }
+
+  for (let i = 0; i < limit; i++) {
+    let firstKey = firstKeys[i];
+    let secondKey = secondKeys[i];
+
+    if (firstKey !== secondKey) {
+      return false;
+    }
+
+    else if (obj1[firstKey] !== obj2[secondKey]) {
+      return false;
+    }
+  }
+
+  return true;
 }
 
 module.exports = { isEqual };
