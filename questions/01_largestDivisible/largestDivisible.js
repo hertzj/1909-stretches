80c460510903b67ad68775b6fe59f0dc4f2c71df
Jake Hertz <hertzjake@gmail.com>
2019-09-27 10:32:40 -0400

    stretch solution largestDivisible

--- questions/01_largestDivisible/largestDivisible.js
+++ questions/01_largestDivisible/largestDivisible.js
@@ -4,7 +4,24 @@ write the largestDivisible function
 
 */
 
-function largestDivisible(arr, num) {}
+function largestDivisible(arr, num) {
+    let largest = null;
+
+    // first let's do this declaratively
+
+    for (let i = 0; i < arr.length; i++) {
+        let potential = arr[i];
+        
+        if (potential % num === 0 && largest === null) {
+            largest = potential;
+        }
+        else if (potential % num === 0 && potential > largest) {
+            largest = potential
+        }
+    }
+
+    return largest;
+}
 
 // console.log(largestDivisible([1, 2, 3, 4, 5, 6, 7], 3));//6
 // console.log(largestDivisible([1], 2));//null
