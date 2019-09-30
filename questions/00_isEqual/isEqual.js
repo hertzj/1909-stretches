12864a04eb7d16d02e7e798b64903b931c55bce8
Jake Hertz <hertzjake@gmail.com>
2019-09-30 18:58:19 -0400

    official FSA solution for weekend exercises

--- questions/00_isEqual/isEqual.js
+++ questions/00_isEqual/isEqual.js
@@ -32,4 +32,20 @@ function isEqual(obj1, obj2) {
   return true;
 }
 
+// FSA solution
+
+function isEqualFsa(obj1, obj2) {
+  const keys1 = Object.keys(obj1);
+  const keys2 = Object.keys(obj2);
+  if (keys1.length !== keys2.length) {
+    return false;
+  }
+  for (let i = 0; i < keys1.length; i++) {
+    if (keys1[i] !== keys2[i]) {
+      return false;
+    }
+  }
+  return true;
+}
+
 module.exports = { isEqual };
