697a0743b1da9ff0dc2a0cd1d2f749a8167e73ad
Jake Hertz <hertzjake@gmail.com>
2019-10-09 18:51:01 -0400

    sixth stretch solution

--- questions/05_Query/query.js
+++ questions/05_Query/query.js
@@ -53,7 +53,7 @@ function query(arr, obj) {
     let key = pair[0];
     let value = pair[1];
     for (let j = 0; j < arr.length; j++) {
-      let item = arr[i];
+      let item = arr[j];
       if (item[key] === value) {
         found.push(item)
       }
@@ -67,5 +67,49 @@ function query(arr, obj) {
 
 }
 
+// FullStack Solution
+
+// sol1
+
+const queryFSA = (arr, obj) => {
+  const filteredObjects = [];
+  for (let i = 0; i < arr.length; i++) {
+    const currentObj = arr[i];
+
+    // user filter object and compare its keys and values with the current object
+    let cleanObj = true;
+
+    for (let k in obj) {
+      const filterValue = obj[k];
+      if (currentObj[k] !== filterValue) {
+        cleanObj = false;
+      }
+    }
+
+    if (cleanObj) {
+      filteredObjects.push(currentObj);
+    }
+  }
+  return filteredObjects;
+}
+
+// sol2
+
+const queryFsaTwo = (arr, obj) => {
+  const filterEntries = Object.entries(obj);
+  
+  return arr.filter(obj => {
+    return filterEntries.every((filterTuple) => {
+      // const key = filterTuple[0];
+      // const value = filterTuple[1];
+      // with destructed syntax
+
+      const [key, value] = filterTuple;
+      // the above is naming a variable key and assigning it to index 0 of filterTuple
+      // and naming a variable value and assigning it to index 1 of filterTuple
+      return obj[key] === value;
+    })
+  })
+}
 
 module.exports = { query, items };
--- questions/06_times/times.js
+++ questions/06_times/times.js
@@ -1,3 +1,14 @@
-function times() {}
+function times(num) {
+    if (typeof num !== 'number') {
+        throw 'Numbers only!'
+    }
+    
+    let priorNum = num;
+    return function newFunc (newNum) {
+        let result = newNum * priorNum;
+        
+        return result;
+    }
+}
 
 module.exports = { times };
