8a05fc381410514db7765541dbde931424889e56
Jake Hertz <hertzjake@gmail.com>
2019-10-16 18:52:29 -0400

    stretch 8 solution

--- questions/07_anagrams/anagrams.js
+++ questions/07_anagrams/anagrams.js
@@ -13,4 +13,50 @@ const checkAnagrams = (str1, str2) => {
 
 };
 
+// FSA solution
+
+const checkAnagramsFSA = (str1, str2) => {
+  if (str1.length !== str2.length) {
+    return false
+  }
+
+  // one solution Russell gives is the sorted solution I came up with above
+  // another is to use an object
+  // the object method runs fewer loops since the array methods
+  // are actually loops
+  // so the object one takes less time
+
+  const letters1 = {};
+  const letters2 = {};
+
+  for (let i = 0; i < str1.length; i++) {
+    const current = str1[i];
+    if (current in letters1) {
+      letters1[current]++
+    }
+    else {
+      letters1[current] = 1
+    }
+  }
+
+  for (let i = 0; i < str2.length; i++) {
+    const current = str2[i];
+    if (current in letters2) {
+      letters2[current]++
+    }
+    else {
+      letters2[current] = 1
+    }
+  }
+
+  for (const letter in letters1) {
+    if (letters1[letter] !== letters2[letter]) {
+      return false
+    }
+  }
+
+  return true;
+
+}
+
 module.exports = { checkAnagrams };
--- questions/08_groupBy/groupBy.js
+++ questions/08_groupBy/groupBy.js
@@ -1,3 +1,34 @@
-function groupBy(arr, attr) {}
+function groupBy(arr, attr) {
+    let group = {}
+    
+    if (typeof attr === 'string') {
+        arr.forEach(element => {
+            let val = element[attr]
+
+            if (val in group) {
+                group[val] = group[val].concat(element)
+            }
+            else {
+                group[val] = [element];
+            }
+        })
+
+        return group
+    }
+
+    else if (typeof attr === 'function') {
+        arr.forEach(element => {
+            let val = attr(element)
+            if (val in group) {
+                group[val] = group[val].concat(element)
+            }
+            else {
+                group[val] = [element]
+            }
+        })
+
+        return group
+    }
+}
 
 module.exports = { groupBy };
