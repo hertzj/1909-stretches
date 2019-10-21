c89c0322faf1b24950487d639a60bb5d4915a3c8
Jake Hertz <hertzjake@gmail.com>
2019-10-21 19:07:10 -0400

    add notes

--- questions/09_trim/trim.js
+++ questions/09_trim/trim.js
@@ -1,24 +1,25 @@
-// const trim = obj => {
-//     const trimmed = {};
-
-//     for (const k in obj) {
-//         if (obj[k] !== undefined && obj[k] !== null) {
-//             trimmed[k] = obj[k]
-//         }
-//     }
-
-//     return trimmed;
-// };
-
 const trim = obj => {
-    const keys = Object.keys(obj);
+    const trimmed = {};
 
-    return keys.reduce((trimmed, current) => {
-        if (obj[current] !== undefined && obj[current] !== null) {
-            trimmed[current] = obj[current]
+    for (const k in obj) {
+        if (obj[k] !== undefined && obj[k] !== null) {
+            trimmed[k] = obj[k]
         }
-        return trimmed
-    }, {})
+    }
+
+    return trimmed;
 };
+
+// const trim = obj => {
+//     const keys = Object.keys(obj);
+
+//     return keys.reduce((trimmed, current) => {
+//         if (obj[current] !== undefined && obj[current] !== null) {
+//             trimmed[current] = obj[current]
+//         }
+//         return trimmed
+//     }, {})
+// };
+// however, Object.keys loops through the entire object, so this solution will take longer as we are iterating through twice compared to once in the original solution
 
 module.exports = { trim };
