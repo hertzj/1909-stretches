5c388d68c99f289fb1821d1eaa62f8efe827103c
Jake Hertz <hertzjake@gmail.com>
2019-10-21 18:49:08 -0400

    trim solution

--- questions/09_trim/trim.js
+++ questions/09_trim/trim.js
@@ -1,3 +1,13 @@
-const trim = obj => {};
+const trim = obj => {
+    const trimmed = {};
+
+    for (const k in obj) {
+        if (obj[k] !== undefined && obj[k] !== null) {
+            trimmed[k] = obj[k]
+        }
+    }
+
+    return trimmed;
+};
 
 module.exports = { trim };
