3bd1235facb7af62973ae08a2932e755d79e35bd
Jake Hertz <hertzjake@gmail.com>
2019-10-21 18:40:38 -0400

    updating

--- questions/08_groupBy/groupBy.js
+++ questions/08_groupBy/groupBy.js
@@ -6,7 +6,7 @@ function groupBy(arr, attr) {
             let val = element[attr]
 
             if (val in group) {
-                group[val] = group[val].concat(element)
+                group[val] = group[val].concat([element])
             }
             else {
                 group[val] = [element];
@@ -20,7 +20,7 @@ function groupBy(arr, attr) {
         arr.forEach(element => {
             let val = attr(element)
             if (val in group) {
-                group[val] = group[val].concat(element)
+                group[val] = group[val].concat([element])
             }
             else {
                 group[val] = [element]
@@ -30,5 +30,22 @@ function groupBy(arr, attr) {
         return group
     }
 }
+
+// FSA Solution
+
+const groupByFsa = (collection, attr) => collection.reduce((groupDict, nextItem) => {
+    let groupDictKey;
+
+    if (typeof attr === 'function') {
+        groupDictKey = attr(nextItem);
+    }
+    else groupDictKey = nextItem[attr];
+
+    groupDict[groupDictKey] = !groupDict[groupDictKey]
+    ? [nextItem]
+    : groupDict[groupDictKey].concat([nextItem]);
+
+    return groupDict
+}, {})
 
 module.exports = { groupBy };
