07af608b8c055b3c6cbc4e20f0c74fa951a02a72
Jake Hertz <hertzjake@gmail.com>
2019-10-28 18:56:59 -0400

    stretch 12 - it did not go well!

--- questions/12_floydsTriangle/floydsTriangle.js
+++ questions/12_floydsTriangle/floydsTriangle.js
@@ -6,6 +6,29 @@
 //  23
 //  456
 
-const floydsTriangle = n => {};
+// really struggled with this one
+const floydsTriangle = n => {
+    // const nums = '1234567890'.split('');
+    let triangle = '';
+
+    // for (let i = 0; i < n; i++) {
+    //     let row = '';
+    //     for (let j = i + 1; j < i + 2; j++) {
+    //         let x = j;
+    //         if (x > 9) {
+    //             x = j % 10;
+    //         }
+    //         let num = nums[x]
+    //         row += num
+    //     }
+    //     triangle += row
+        
+    // }
+
+
+
+    return triangle
+
+};
 
 module.exports = { floydsTriangle };
