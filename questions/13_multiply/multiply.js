e3621f4b4167ea5da6a354e5f21fd23c004f4ebb
Jake Hertz <hertzjake@gmail.com>
2019-10-30 18:50:40 -0400

    refactor tests

--- questions/13_multiply/multiply.js
+++ questions/13_multiply/multiply.js
@@ -2,7 +2,7 @@ const multiply = (num1, num2) => {
     if (typeof num1 !== 'number' || typeof num2 !== 'number') {
         throw new Error('you must enter two numbers!');
     };
-    return num1 * num2
+    return num1 * num2;
 };
 
 module.exports = { multiply };
--- questions/13_multiply/multiply.test.js
+++ questions/13_multiply/multiply.test.js
@@ -11,19 +11,22 @@ describe('multiply testing', () => {
 
   test('it returns the product of two numbers', () => {
     // test the type of the returned value
-    expect(multiply(2,3)).toBe(6);
+    const test1 = multiply(2,3);
+    expect(test1).toBe(6);
 
     // test that the returned value is correct
-    expect(multiply(2, -1)).toBe(-2);
+    const test2 = multiply(2, -1);
+    expect(test2).toBe(-2);
 
     // test some other values
-    expect(multiply(17, 10)).toBe(170);
+    const test3 = multiply(17, 10)
+    expect(test3).toBe(170);
   });
 
   test('it is not hardcoded (hint: use random numbers)', () => {
     const testNum1 = Math.random() * 100;
     const testNum2 = Math.random() * 100;
-    const testMult = multiply(testNum1, testNum2)
+    const testMult = multiply(testNum1, testNum2);
     const product = testNum1 * testNum2;
     expect(testMult).toBe(product);
   });
