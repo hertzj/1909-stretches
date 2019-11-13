c2ee974927d848e8513ebfe3eef547d05469e123
Jake Hertz <hertzjake@gmail.com>
2019-11-13 18:55:10 -0500

    not quite

--- questions/15_cache/cache.js
+++ questions/15_cache/cache.js
@@ -1,3 +1,17 @@
-function cache() {}
+function cache(func) {
+    if (typeof func !== 'function') {
+        throw new Error('Input must be a function.');
+    }
+
+    let args = [];
+    let argument = arguments[0];
+    return function() {
+        if (args.indexOf(argument) === -1 ) {
+            args.push(argument);
+            return func
+        }
+    }
+
+}
 
 module.exports = { cache };
