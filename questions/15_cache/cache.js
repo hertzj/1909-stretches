a5b09e566183ce74ece0c4295dfa846fdbd1ebe8
Jake Hertz <hertzjake@gmail.com>
2019-11-18 18:40:53 -0500

    aligning

--- questions/15_cache/cache.js
+++ questions/15_cache/cache.js
@@ -5,13 +5,29 @@ function cache(func) {
 
     let args = [];
     let argument = arguments[0];
+    args.push(argument);
     return function() {
         if (args.indexOf(argument) === -1 ) {
-            args.push(argument);
             return func
         }
     }
 
+}
+
+// FSA solution
+function cacheFsa(cb) {
+    if (typeof cb !== 'function') throw new Error ('Input must be a function.');
+    
+    const memo = {}
+
+    return function cached(...n) {
+        if (memo[n] !== undefined) {
+            return memo[n];
+        } else {
+            memo[n] = cb(...n)
+            return memo[n];
+        }
+    }
 }
 
 module.exports = { cache };
