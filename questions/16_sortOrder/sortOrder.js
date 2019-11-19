fc1269c9889d83fb1b4e5535f89da3653eb0ec07
Jake Hertz <hertzjake@gmail.com>
2019-11-18 19:00:35 -0500

    failed this one!

--- questions/16_sortOrder/sortOrder.js
+++ questions/16_sortOrder/sortOrder.js
@@ -3,6 +3,14 @@ SortOrder takes an array of objects, a key to be sorted on
 and a key for the return value. 
 */
 
-function sortOrder() {}
+function sortOrder(arrOfObj, sortKey, retKey) {
+    const newArr = arrOfObj.map(obj => Object.entries(obj));
+    return newArr.sort;
+    // ran out of time
+    
+};
+
+
+
 
 module.exports = { sortOrder };
